6ab624e10dd161459ce8ffd45d828ae1b7afd051
jkablan <jkablan@kernix.com>
2019-06-12 17:00:38 +0200

    test 2

--- symfony/src/Admin/MainTest.php
+++ symfony/src/Admin/MainTest.php
@@ -3,13 +3,8 @@
 namespace App\Admin;
 
 use Sonata\AdminBundle\Admin\AbstractAdmin;
-use Sonata\AdminBundle\Datagrid\ListMapper;
-use Sonata\AdminBundle\Datagrid\DatagridMapper;
-use Sonata\AdminBundle\Form\FormMapper;
-use Sonata\AdminBundle\Route\RouteCollection;
-use Sonata\CoreBundle\Form\Type\BooleanType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class MainTest extends AbstractAdmin
 {
+    public function ;
+}
